b3bbc70f7d4980166886da379dff99e45c1cbf98
Phil Hawker <philhawker1211@gmail.com>
2020-01-29 00:16:09 -0700

    inital commit

--- pollster/polls/views.py
+++ pollster/polls/views.py
@@ -6,33 +6,25 @@ from django.urls import reverse
 from .models import Question, Choice
 
 # Get questions and display them
-
-
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
     context = {'latest_question_list': latest_question_list}
     return render(request, 'polls/index.html', context)
 
 # Show specific question and choices
-
-
 def detail(request, question_id):
   try:
     question = Question.objects.get(pk=question_id)
   except Question.DoesNotExist:
     raise Http404("Question does not exist")
-  return render(request, 'polls/detail.html', {'question': question})
+  return render(request, 'polls/detail.html', { 'question': question })
 
 # Get question and display results
-
-
 def results(request, question_id):
   question = get_object_or_404(Question, pk=question_id)
-  return render(request, 'polls/results.html', {'question': question})
+  return render(request, 'polls/results.html', { 'question': question })
 
 # Vote for a question choice
-
-
 def vote(request, question_id):
     # print(request.POST['choice'])
     question = get_object_or_404(Question, pk=question_id)
